e388a9473b03dba63d742f1b477704e966c88e64
naiwu <aungnaingoo2005@qq.com>
2025-03-14 18:30:08 +0800

    update

--- aung/第一步.py
+++ aung/第一步.py
@@ -31,3 +31,38 @@ cropped_image = image[new_height_start:new_height_end, width//2:]
 # 保存裁剪后的图像
 cv2.imwrite('cropped_apple.png', cropped_image)
 
+sketch_apple.py
+from PIL import Image
+import numpy as np
+
+
+def convert_to_sketch(image_path):
+    original_image = Image.open(image_path).convert("L")  # 打开图片, 并转换为灰度图
+
+    img_array = np.array(original_image)  # 将图片转换为 np数组
+
+    # 创建一个空的画布用于素描图
+    sketch_img = np.zeros_like(img_array)  # 用 np 创建一个跟 img_array 数组模型一样的全 0 数组
+
+    # 计算梯度
+    for i in range(1, img_array.shape[0] - 1):
+        for j in range(1, img_array.shape[1] - 1):  # 遍历图像的每个像素点 (按照坐标轴看 i是y j是x)
+            gx = int(img_array[i, j + 1]) - int(img_array[i, j - 1])  # 计算x方向梯度
+            gy = int(img_array[i + 1, j]) - int(img_array[i - 1, j])  # 计算y方向梯度
+            magnitude = np.sqrt(gx ** 2 + gy ** 2)  # 计算梯度幅值
+            sketch_img[i, j] = magnitude  # 将梯度幅值赋给sketch_img
+
+    # 反转颜色以获得素描效果
+    sketch_img = 255 - sketch_img  # 因为全 0 数组是黑色的，所以这里用 255 相减得反色
+
+    # 将numpy数组转换回PIL图像
+    sketch_image = Image.fromarray(sketch_img.astype(np.uint8))
+
+    return sketch_image
+
+
+input_image_path = "cropped_apple_no_bg.png"
+output_image_path = "cropped_apple_sketch.png"
+
+sketch_image = convert_to_sketch(input_image_path)
+sketch_image.save(output_image_path)  # 保存图片
