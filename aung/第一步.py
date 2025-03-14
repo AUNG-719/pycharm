68c03cea4d25aeb49ea4d7c514fee6ec950cc97d
naiwu <aungnaingoo2005@qq.com>
2025-03-14 18:25:11 +0800

    update

---
+++ aung/第一步.py
@@ -0,0 +1,19 @@
+
+import cv2
+
+# 读取图像
+image = cv2.imread('apple.png')
+
+# 获取图像的高度和宽度
+height, width = image.shape[:2]
+
+# 计算新的高度范围
+new_height_start = height // 3  # 从上面裁剪掉1/3
+new_height_end = height  # 留下面2/3的部分
+
+# 裁剪图像宽度的一半，并且裁剪高度的下2/3部分
+cropped_image = image[new_height_start:new_height_end, width//2:]
+
+# 保存裁剪后的图像
+cv2.imwrite('cropped_apple.png', cropped_image)
+
