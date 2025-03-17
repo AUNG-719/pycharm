e8d2cc1bc98e88a5e4275fa97ab7c57cebd03a4e
AUNG-719 <aungnaingoo2005@qq.com>
2025-03-17 18:31:31 +0800

    Update python作业.py

--- aung/python作业.py
+++ aung/python作业.py
@@ -1,3 +1,8 @@
 fahrenheit = float(input("输入一个华氏温度："))
 celsiust = 5/9*(fahrenheit-32)
 print("摄氏温度值为：",celsiust)
+
+print("今天是星期4，求第n天之后是星期几？")
+n = int(input("输入n的值:"))
+weekday = (4*n)%7
+print(n,"天之后，星期",weekday)
